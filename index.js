386b555f380aeaad71a0e6c0089f706b3dc74948
Dustin Ruskell <druskell@gmail.com>
2018-08-17 14:27:01 -0700

    added a read me

--- index.js
+++ index.js
@@ -48,21 +48,21 @@ function dateObj() {
       }
     }
     if (working > date) {
-      counter++
-      result[yearCog][monthCog][weekCog][dayCog]['count'] = counter
+      counter++;
+      result[yearCog][monthCog][weekCog][dayCog]['count'] = counter;
     } else {
-      result[yearCog][monthCog][weekCog][dayCog]['count'] = ((parseInt(working.format('DD')) - parseInt(date.format('DD'))))
+      result[yearCog][monthCog][weekCog][dayCog]['count'] = ((parseInt(working.format('DD')) - parseInt(date.format('DD'))));
     }
-    working = working.add(1, 'days')
+    working = working.add(1, 'days');
   }
-  return result
+  return result;
 }
 
 function topFill() {
 
-  let plusThirty = daySelected.clone().add(30, 'days').format()
-  let plusSixty = daySelected.clone().add(60, 'days').format()
-  let plusNinety = daySelected.clone().add(90, 'days').format()
+  let plusThirty = daySelected.clone().add(30, 'days').format();
+  let plusSixty = daySelected.clone().add(60, 'days').format();
+  let plusNinety = daySelected.clone().add(90, 'days').format();
 
   // Pushing the current date to the BIGBOX
   document.querySelector('#thisMonth').innerHTML = month;
@@ -70,260 +70,228 @@ function topFill() {
   document.querySelector('#thisYear').innerHTML = year;
 
   // Pushing the set days to the NARROWBOX
-  let thirtyDay = document.querySelector('.thirtyDay')
-  let thirtyMonth = document.querySelector('.thirtyMonth')
-  let thirtyYear = document.querySelector('.thirtyYear')
+  let thirtyDay = document.querySelector('.thirtyDay');
+  let thirtyMonth = document.querySelector('.thirtyMonth');
+  let thirtyYear = document.querySelector('.thirtyYear');
 
-  thirtyDay.innerHTML = moment.utc(plusThirty).format('DD')
-  thirtyMonth.innerHTML = moment.utc(plusThirty).format('MMMM')
-  thirtyYear.innerHTML = moment.utc(plusThirty).format('YYYY')
+  thirtyDay.innerHTML = moment.utc(plusThirty).format('DD');
+  thirtyMonth.innerHTML = moment.utc(plusThirty).format('MMMM');
+  thirtyYear.innerHTML = moment.utc(plusThirty).format('YYYY');
 
-  let sixtyDay = document.querySelector('.sixtyDay')
-  sixtyDay.innerHTML = moment.utc(plusSixty).format('DD')
-  let sixtyMonth = document.querySelector('.sixtyMonth')
-  sixtyMonth.innerHTML = moment.utc(plusSixty).format('MMMM')
-  let sixtyYear = document.querySelector('.sixtyYear')
-  sixtyYear.innerHTML = moment.utc(plusSixty).format('YYYY')
+  let sixtyDay = document.querySelector('.sixtyDay');
+  sixtyDay.innerHTML = moment.utc(plusSixty).format('DD');
+  let sixtyMonth = document.querySelector('.sixtyMonth');
+  sixtyMonth.innerHTML = moment.utc(plusSixty).format('MMMM');
+  let sixtyYear = document.querySelector('.sixtyYear');
+  sixtyYear.innerHTML = moment.utc(plusSixty).format('YYYY');
 
-  let ninetyDay = document.querySelector('.ninetyDay')
-  ninetyDay.innerHTML = moment.utc(plusNinety).format('DD')
-  let ninetyMonth = document.querySelector('.ninetyMonth')
-  ninetyMonth.innerHTML = moment.utc(plusNinety).format('MMMM')
-  let ninetyYear = document.querySelector('.ninetyYear')
-  ninetyYear.innerHTML = moment.utc(plusNinety).format('YYYY')
+  let ninetyDay = document.querySelector('.ninetyDay');
+  ninetyDay.innerHTML = moment.utc(plusNinety).format('DD');
+  let ninetyMonth = document.querySelector('.ninetyMonth');
+  ninetyMonth.innerHTML = moment.utc(plusNinety).format('MMMM');
+  let ninetyYear = document.querySelector('.ninetyYear');
+  ninetyYear.innerHTML = moment.utc(plusNinety).format('YYYY');
 
-  let monthTitle = document.querySelector('.monthTitle')
-  monthTitle.innerHTML = month
-  let yearTitle = document.querySelector('.yearTitle')
-  yearTitle.innerHTML = year
+  let monthTitle = document.querySelector('.monthTitle');
+  monthTitle.innerHTML = month;
+  let yearTitle = document.querySelector('.yearTitle');
+  yearTitle.innerHTML = year;
 }
-topFill()
+topFill();
 
 function deletePreviousCalendar() {
-  let toDelete = document.querySelectorAll('.toDelete') || []
+  let toDelete = document.querySelectorAll('.toDelete') || [];
 
   for (let deleted = 0; deleted < toDelete.length; deleted++) {
 
-    toDelete[deleted].parentElement.removeChild(toDelete[deleted])
+    toDelete[deleted].parentElement.removeChild(toDelete[deleted]);
   }
 }
 
 function htmlBuilder(months, years) {
   //clone the month
-  let parent = document.querySelector('.container')
-  let monthBox = document.querySelector('.monthBox')
-  let newBox = monthBox.cloneNode(true)
-  newBox.classList.remove('hidden')
-  newBox.classList.add('toDelete')
-  parent.appendChild(newBox)
+  let parent = document.querySelector('.container');
+  let monthBox = document.querySelector('.monthBox');
+  let newBox = monthBox.cloneNode(true);
+  newBox.classList.remove('hidden');
+  newBox.classList.add('toDelete');
+  parent.appendChild(newBox);
 }
 
 function titles(newBox, months, years) {
   // build the titleRow
-  let titleRow = document.querySelector('.titleRow')
-  let titleBox = document.querySelector('.titleBox')
+  let titleRow = document.querySelector('.titleRow');
+  let titleBox = document.querySelector('.titleBox');
   for (let title of daysOfTheWeek) {
-    let newTitle = titleBox.cloneNode(true)
-    newTitle.innerHTML = title.slice(0, 3)
-    newTitle.classList.remove('hidden')
-    newBox.querySelector('.titleRow').appendChild(newTitle)
+    let newTitle = titleBox.cloneNode(true);
+    newTitle.innerHTML = title.slice(0, 3);
+    newTitle.classList.remove('hidden');
+    newBox.querySelector('.titleRow').appendChild(newTitle);
   }
 
-
-
-
-
-  //this could be its own functions seperate from above.
   let monthText = newBox.firstElementChild.firstElementChild
-  let yearText = newBox.firstElementChild.children[1]
-  monthText.innerHTML = '<h1>' + moment(months, 'MM').format('MMMM') + '</h1>'
-  yearText.innerHTML = '<h1>' + years + '</h1>'
+  let yearText = newBox.firstElementChild.children[1];
+  monthText.innerHTML = '<h1>' + moment(months, 'MM').format('MMMM') + '</h1>';
+  yearText.innerHTML = '<h1>' + years + '</h1>';
 }
 
 function theFirstIs(workingDate) {
   if (workingDate['01']) {
-    let theFirstIsA = workingDate['01']['dayName']
-    return theFirstIsA
+    let theFirstIsA = workingDate['01']['dayName'];
+    return theFirstIsA;
   }
 }
 
 function blankBuilder(theFirstIsA, newBox) {
   //build the blank boxes at the beginning of the month
-  let blankBox = document.querySelector('.blankBox')
-  let newWeekRow = newBox.querySelector('.weekRow')
+  let blankBox = document.querySelector('.blankBox');
+  let newWeekRow = newBox.querySelector('.weekRow');
 
   for (let blankMaker = 0; blankMaker < daysOfTheWeek.indexOf(theFirstIsA); blankMaker++) {
-    let blankBuild = blankBox.cloneNode(true)
-    blankBuild.classList.remove('hidden')
-    newWeekRow.appendChild(blankBuild)
+    let blankBuild = blankBox.cloneNode(true);
+    blankBuild.classList.remove('hidden');
+    newWeekRow.appendChild(blankBuild);
   }
 }
 
 function dayBuilder(workingDate, newBox) {
   //build days that aren't blank, and subsequent weekDays
-  let newWeekRow = newBox.querySelector('.weekRow')
-  // console.log(workingDate)
-  let dayArr = Object.keys(workingDate).sort()
+  let newWeekRow = newBox.querySelector('.weekRow');
+  let dayArr = Object.keys(workingDate).sort();
   for (let thisDay of dayArr) {
 
-    let dayInQuestion = workingDate[thisDay].dayName
-    let contentBox = document.querySelector('.fillBox')
-    let contentBuilder = contentBox.cloneNode('true')
-    let calendarContent = contentBuilder.querySelector('.calendarDate')
-    let countContent = contentBuilder.querySelector('.dayDisplay')
-    calendarContent.innerHTML = thisDay
-    countContent.innerHTML = workingDate[thisDay].count
-    //HIDDEN COULD BE A FUNCTION
-    contentBuilder.classList.remove('hidden')
-    //COULD BE A FUNCTION
+    let dayInQuestion = workingDate[thisDay].dayName;
+    let contentBox = document.querySelector('.fillBox');
+    let contentBuilder = contentBox.cloneNode('true');
+    let calendarContent = contentBuilder.querySelector('.calendarDate');
+    let countContent = contentBuilder.querySelector('.dayDisplay');
+    calendarContent.innerHTML = thisDay;
+    countContent.innerHTML = workingDate[thisDay].count;
+    contentBuilder.classList.remove('hidden');
     if (countContent.innerHTML % 15 === 0) {
-      contentBuilder.classList.add('yellow')
+      contentBuilder.classList.add('yellow');
     }
     if (countContent.innerHTML % 30 === 0) {
       contentBuilder.classList.add('blue')
     }
     if (countContent.innerHTML == 0) {
-      contentBuilder.classList.add('green')
+      contentBuilder.classList.add('green');
     }
-    //THIS COULD BE ITS OWN FUNCTION
     //If Sunday make a new row
 
-    let weekInsert = newBox.querySelector('.insertRows')
-    let weekRow = document.querySelector('.weekRow')
-    let nextWeekRow = weekRow.cloneNode(true)
-    let blankBox = document.querySelector('.blankBox')
+    let weekInsert = newBox.querySelector('.insertRows');
+    let weekRow = document.querySelector('.weekRow');
+    let nextWeekRow = weekRow.cloneNode(true);
+    let blankBox = document.querySelector('.blankBox');
     if (dayInQuestion === daysOfTheWeek[0] && dayArr.length < 7 && dayInQuestion === workingDate[dayArr[dayArr.length-1]].dayName && dayArr[dayArr.length - 1] > 27){
-      nextWeekRow.appendChild(contentBuilder)
-      weekInsert.appendChild(nextWeekRow)
-      newWeekRow = nextWeekRow
-      newWeekRow.appendChild(contentBuilder)
-      let daysMadeInLastWeek = newWeekRow.children
+      nextWeekRow.appendChild(contentBuilder);
+      weekInsert.appendChild(nextWeekRow);
+      newWeekRow = nextWeekRow;
+      newWeekRow.appendChild(contentBuilder);
+      let daysMadeInLastWeek = newWeekRow.children;
       for (let soFar = daysMadeInLastWeek.length; soFar < 9; soFar++) {
-        let newBlankBuilder = blankBox.cloneNode(true)
-        newBlankBuilder.classList.remove('hidden')
-        newWeekRow.appendChild(newBlankBuilder.cloneNode(true))
+        let newBlankBuilder = blankBox.cloneNode(true);
+        newBlankBuilder.classList.remove('hidden');
+        newWeekRow.appendChild(newBlankBuilder.cloneNode(true));
       }
     } else if(dayInQuestion === daysOfTheWeek[0]) {
-      nextWeekRow.appendChild(contentBuilder)
-      weekInsert.appendChild(nextWeekRow)
-      newWeekRow = nextWeekRow
+      nextWeekRow.appendChild(contentBuilder);
+      weekInsert.appendChild(nextWeekRow);
+      newWeekRow = nextWeekRow;
     } else if (dayArr.length < 7 && dayInQuestion === workingDate[dayArr[dayArr.length-1]].dayName && dayArr[dayArr.length - 1] > 27) {
-        newWeekRow.appendChild(contentBuilder)
-        let daysMadeInLastWeek = newWeekRow.children
+        newWeekRow.appendChild(contentBuilder);
+        let daysMadeInLastWeek = newWeekRow.children;
         for (let soFar = daysMadeInLastWeek.length; soFar < 9; soFar++) {
-          let newBlankBuilder = blankBox.cloneNode(true)
-          newBlankBuilder.classList.remove('hidden')
-          newWeekRow.appendChild(newBlankBuilder.cloneNode(true))
+          let newBlankBuilder = blankBox.cloneNode(true);
+          newBlankBuilder.classList.remove('hidden');
+          newWeekRow.appendChild(newBlankBuilder.cloneNode(true));
       }
     } else {
-        newWeekRow.appendChild(contentBuilder)
+        newWeekRow.appendChild(contentBuilder);
     }
   }
 }
 
 function removeIncompleteMonth () {
-  let parent = document.querySelector('.container')
-  let last = parent.children[parent.children.length - 1]
-  last.parentElement.removeChild(last)
+  let parent = document.querySelector('.container');
+  let last = parent.children[parent.children.length - 1];
+  last.parentElement.removeChild(last);
 }
-
-
-
-
-
-
-
-
-
-
-
 
 // Build the calendars by cloning elements and populating the data
 function CalendarBuilder() {
-  deletePreviousCalendar()
+  deletePreviousCalendar();
 
-  let dateObjCycler = dateObj()
+  let dateObjCycler = dateObj();
 
 
   // Pick out the years
   for (let years in dateObjCycler) {
     // Pick out the months, and order them properly
     let monthArr = Object.keys(dateObjCycler[years]).sort()
-    // cycle through the months and build a new calendar month for each
     for (let months of monthArr) {
 
-      htmlBuilder(months, years)
+      htmlBuilder(months, years);
       const newBox = document.querySelector('.container').lastElementChild;
-      titles(newBox, months, years)
+      titles(newBox, months, years);
 
-      let weekArr = Object.keys(dateObjCycler[years][months]).sort()
+      let weekArr = Object.keys(dateObjCycler[years][months]).sort();
       if(weekArr[0] === '01' && weekArr[weekArr.length-1] > 50) {
-        let yearEndWeekAdjustment = weekArr.shift()
-        weekArr.push(yearEndWeekAdjustment)
+        let yearEndWeekAdjustment = weekArr.shift();
+        weekArr.push(yearEndWeekAdjustment);
       }
       for(let weeks of weekArr) {
 
-        const workingDate = dateObjCycler[years][months][weeks]
+        const workingDate = dateObjCycler[years][months][weeks];
 
-        let theFirst = theFirstIs(workingDate)
+        let theFirst = theFirstIs(workingDate);
 
         // build the weeks of the month
-        blankBuilder(theFirst, newBox)
-        dayBuilder(workingDate, newBox)
+        blankBuilder(theFirst, newBox);
+        dayBuilder(workingDate, newBox);
       }
     }
   }
-  removeIncompleteMonth ()
+  removeIncompleteMonth ();
 }
-CalendarBuilder()
-
-
-
-
-
-
-
-
-
+CalendarBuilder();
 
 function reset() {
 
   let replacementDate = new Date();
-  datePicker.valueAsDate = replacementDate
-  date = moment.utc(datePicker.valueAsDate)
-  daySelected = date.clone()
-  localStorage.removeItem('datePicked')
+  datePicker.valueAsDate = replacementDate;
+  date = moment.utc(datePicker.valueAsDate);
+  daySelected = date.clone();
+  localStorage.removeItem('datePicked');
 
   let day = daySelected.format('DD');
   let monthNum = daySelected.format('MM');
-  let month = daySelected.format('MMMM')
+  let month = daySelected.format('MMMM');
   let year = daySelected.year();
-  let plusThirty = daySelected.clone().add(30, 'days').format()
-  let plusSixty = daySelected.clone().add(60, 'days').format()
-  let plusNinety = daySelected.clone().add(90, 'days').format()
-  topFill()
-  CalendarBuilder()
+  let plusThirty = daySelected.clone().add(30, 'days').format();
+  let plusSixty = daySelected.clone().add(60, 'days').format();
+  let plusNinety = daySelected.clone().add(90, 'days').format();
+  topFill();
+  CalendarBuilder();
 }
 
 //start of update function
 document.getElementById('datePicker').addEventListener('change', () => {
 
-  date = moment.utc(datePicker.valueAsDate)
-  daySelected = moment.utc(date.clone())
-  localStorage.setItem('datePicked', JSON.stringify(moment(date).format('YYYY-MM-DD')))
-  // console.log('brand new',moment.utc(daySelected.add(30,'days ')).format('ddd'))
+  date = moment.utc(datePicker.valueAsDate);
+  daySelected = moment.utc(date.clone());
+  localStorage.setItem('datePicked', JSON.stringify(moment(date).format('YYYY-MM-DD')));
 
   day = daySelected.format('DD');
   monthNum = daySelected.format('MM');
-  month = daySelected.format('MMMM')
+  month = daySelected.format('MMMM');
   year = daySelected.year();
 
-  plusThirty = daySelected.clone().add(30, 'days').format()
-  plusSixty = daySelected.clone().add(60, 'days').format()
-  plusNinety = daySelected.clone().add(90, 'days').format()
+  plusThirty = daySelected.clone().add(30, 'days').format();
+  plusSixty = daySelected.clone().add(60, 'days').format();
+  plusNinety = daySelected.clone().add(90, 'days').format();
 
-  topFill()
+  topFill();
 
-  CalendarBuilder()
-
+  CalendarBuilder();
 })
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Burckhart Day counter
+
+A functional calendaring tool that displays how far days are from a selected day.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+Run the website link:  [Burckhart Day Counter](soft-camera.surge.sh)
+
+1. Select the day you want to be the start date.
+2. Review the preselected 30, 40, or 90 days at the top of the list
+3. Search Calendar below the Jumbotron to find the day you want
+
+### Prerequisites
+
+Allow your browser to run JS.
+
+### Installing
+
+No installation needed!
+
+## Deployment
+
+Code is deployed on surge
+
+## Built With
+
+* [JS](https://www.javascript.com/) - Javascript
+
+## Authors
+
+* **Dustin Ruskell** - *Initial work* - [GTLook](https://github.com/Ventronik)
+
+## License
+
+N/A
+
+## Acknowledgments
+
+* Would of not been possible without the timeless patience from the Galvanize instructors
